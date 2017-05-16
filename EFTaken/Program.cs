2cbf17d7cf839f98f4bada6653b7fb5951f68e73
EllenFeyen <ellenfeyen@outlook.com>
2017-05-16 08:56:03 +0200

    Add project files.

---
+++ EFTaken/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Transactions;
+using System.Data.Entity.Infrastructure;
+
+namespace EFTaken
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.Write("Klantnr.:");
+                var klantNr = int.Parse(Console.ReadLine());
+
+                using (var entities = new BankEntities())
+                {
+                    var klant = entities.Klanten.Find(klantNr);
+                    if (klant == null)
+                    { Console.WriteLine("Klant niet gevonden"); }
+                    else
+                    {
+                        Console.Write("Geef de juiste voornaam:");
+                        var voornaam = Console.ReadLine();
+                        klant.Voornaam = voornaam;
+                        entities.SaveChanges();
+                    }
+                }
+            }
+            catch (DbUpdateConcurrencyException)
+            { Console.WriteLine("Een andere gebruiker wijzigde deze klant"); }
+            catch (FormatException)
+            { Console.WriteLine("Tik een getal"); }
+
+            Console.WriteLine("Druk enter om af te sluiten");
+            Console.Read();
+        }
+    }
+}
---
+++ EFTaken/RekeningUitbreiding.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EFTaken
+{
+    public partial class Rekening
+    {
+        public void Storten(decimal bedrag)
+        {
+            this.Saldo += bedrag;
+        }
+        public void Overschrijven(Rekening naarRekening, decimal bedrag)
+        {
+            if (this.Saldo < bedrag)
+            {
+                throw new SaldoOntoereikendException();
+            }
+            else
+            {
+                this.Saldo -= bedrag;
+                naarRekening.Saldo += bedrag;
+            }
+        }
+    }
+}
